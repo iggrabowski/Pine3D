1ef950054e3b5ea7d047bf62b93aabb8013af522
iggrabowski <39482312+iggrabowski@users.noreply.github.com>
2026-05-11 20:15:47 +0200

    formatting changes

--- src/Platform/OpenGL/OpenGLRenderer.cpp
+++ src/Platform/OpenGL/OpenGLRenderer.cpp
@@ -471,8 +471,7 @@ OpenGLRenderer::OpenGLRenderer() {
   glEnable(GL_CULL_FACE);
 }
 
-OpenGLRenderer::~OpenGLRenderer()
-{
+OpenGLRenderer::~OpenGLRenderer() {
   glDeleteTextures(1, &_skyboxTextureObj);
   glDeleteVertexArrays(1, &_skyboxCubeVAO);
   glDeleteTextures(1, &_irradianceMapTextureObj);
@@ -597,8 +596,10 @@ void OpenGLRenderer::GenerateFrameBuffers() {
   }
   glGenRenderbuffers(1, &_captureRBO);
   glBindRenderbuffer(GL_RENDERBUFFER, _captureRBO);
-  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, windowSize.x, windowSize.y);
-  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _captureRBO);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, windowSize.x,
+                        windowSize.y);
+  glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT,
+                            GL_RENDERBUFFER, _captureRBO);
 
   if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
     std::cerr << "Framebuffer not complete!" << std::endl;
@@ -661,7 +662,8 @@ void OpenGLRenderer::UpdateRenderDimensions() {
   ResizeTexture(_pingpongCBO[1], newSize.x, newSize.y);
 
   glBindRenderbuffer(GL_RENDERBUFFER, _captureRBO);
-  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, newSize.x, newSize.y);
+  glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, newSize.x,
+                        newSize.y);
   glBindRenderbuffer(GL_RENDERBUFFER, 0);
 }
 
@@ -682,11 +684,11 @@ void OpenGLRenderer::BlendColorBuffers(GLuint target, GLuint source) {
 }
 
 void OpenGLRenderer::RenderMesh(MeshRenderer *mr) {
-   unsigned int attachments[2] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
+  unsigned int attachments[2] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
   glDrawBuffers(2, attachments);
 
   // TODO: Draw can be abstracted
-  Model3D* model = mr->GetModel();
+  Model3D *model = mr->GetModel();
   if (!model->mesh._buffered) {
     // TODO: this is wrong, needs seperate mesh/shader
     // BufferMesh(model.mesh, *mat.m_Shader);
@@ -696,20 +698,20 @@ void OpenGLRenderer::RenderMesh(MeshRenderer *mr) {
 
   // bind VAO once for the mesh data
   glBindVertexArray(
-    model->mesh.m_vertexArrayObject); // DIFFERENT FROM _VA BUFFERS
+      model->mesh.m_vertexArrayObject); // DIFFERENT FROM _VA BUFFERS
 
   // TODO OPT: redundant operations
   mat4 umodel = mr->GetTransform().GetModel();
   mat4 mvp =
-    Application::renderer->GetRenderCamera().GetViewProjection() * umodel;
+      Application::renderer->GetRenderCamera().GetViewProjection() * umodel;
   vec3 camPos = Application::renderer->GetRenderCamera().GetPos();
 
   // TODO : check if window open cause of crashes
   // If model defines BasicMesh ranges, draw each range separately.
   if (!model->b_meshes.empty()) {
-    for (const BasicMesh& bm : model->b_meshes) {
+    for (const BasicMesh &bm : model->b_meshes) {
       // bind shader/texture (existing code assumes 'mat' is set up)
-      Shader* shader = model->materials[bm.materialIndex]->m_shader;
+      Shader *shader = model->materials[bm.materialIndex]->m_shader;
       shader->Bind();
       // material->m_Shader->SetUniform("Model", model);
       //  TODO: think if shader attributes should be selected from shader data
@@ -720,31 +722,31 @@ void OpenGLRenderer::RenderMesh(MeshRenderer *mr) {
       // shader->SetUniform("u_lightDir", lightDir);
       // shader->SetUniform("u_lightColor", lightColor);
       shader->SetUniform("u_roughness",
-        model->materials[bm.materialIndex]->m_roughness);
+                         model->materials[bm.materialIndex]->m_roughness);
       shader->SetUniform("u_metalness",
-        model->materials[bm.materialIndex]->m_metallic);
+                         model->materials[bm.materialIndex]->m_metallic);
       shader->SetUniform("u_renderFlags", mr->m_render_flags[bm.materialIndex]);
       shader->SetUniform("u_prefilteredMapMaxLOD",
-        _prefilteredMapMaxMipLevels - 1);
+                         _prefilteredMapMaxMipLevels - 1);
 
       // lights
       unsigned int lightCount = 0;
       std::vector<vec3> lightDirs;
       std::vector<vec3> lightColors;
       if (!Application::lightPresets.empty() &&
-        std::cmp_less(Application::activeLightPresetIndex,
-          Application::lightPresets.size())) {
+          std::cmp_less(Application::activeLightPresetIndex,
+                        Application::lightPresets.size())) {
         lightCount = static_cast<unsigned int>(
-          Application::lightPresets[Application::activeLightPresetIndex]
-          .size());
+            Application::lightPresets[Application::activeLightPresetIndex]
+                .size());
 
         for (unsigned int i = 0; i < lightCount; ++i) {
           lightDirs.push_back(
-            Application::lightPresets[Application::activeLightPresetIndex][i]
-            .GetDirection());
+              Application::lightPresets[Application::activeLightPresetIndex][i]
+                  .GetDirection());
           lightColors.push_back(
-            Application::lightPresets[Application::activeLightPresetIndex][i]
-            .GetColor());
+              Application::lightPresets[Application::activeLightPresetIndex][i]
+                  .GetColor());
         }
       }
 
@@ -767,53 +769,53 @@ void OpenGLRenderer::RenderMesh(MeshRenderer *mr) {
       // Bind albedo (base) to texture unit 0
       // TODO: hardcoded texture unit indexes, also make functions for redundant
       // uniform setting
-      auto* albedoTex =
-        model->materials[bm.materialIndex]->m_textures[TEX_TYPE_BASE];
+      auto *albedoTex =
+          model->materials[bm.materialIndex]->m_textures[TEX_TYPE_BASE];
       if (albedoTex && mr->m_render_flags[bm.materialIndex] &
-        static_cast<uint32_t>(RenderFlags::BASE_TEXTURE)) {
+                           static_cast<uint32_t>(RenderFlags::BASE_TEXTURE)) {
         albedoTex->Bind(3);
         shader->SetUniformTextureSampler2D("u_albedoMap", 3);
       }
 
       // Bind normal map to texture unit 1
-      auto* normalTex =
-        model->materials[bm.materialIndex]->m_textures[TEX_TYPE_NORMAL];
+      auto *normalTex =
+          model->materials[bm.materialIndex]->m_textures[TEX_TYPE_NORMAL];
       if (normalTex && mr->m_render_flags[bm.materialIndex] &
-        static_cast<uint32_t>(RenderFlags::NORMAL_MAPS)) {
+                           static_cast<uint32_t>(RenderFlags::NORMAL_MAPS)) {
         normalTex->Bind(4);
         shader->SetUniformTextureSampler2D("u_normalMap", 4);
       }
 
-      auto* roughnessTex =
-        model->materials[bm.materialIndex]->m_textures[TEX_TYPE_ROUGHNESS];
+      auto *roughnessTex =
+          model->materials[bm.materialIndex]->m_textures[TEX_TYPE_ROUGHNESS];
       if (roughnessTex &&
-        mr->m_render_flags[bm.materialIndex] &
-        static_cast<uint32_t>(RenderFlags::ROUGHNESS_MAPS)) {
+          mr->m_render_flags[bm.materialIndex] &
+              static_cast<uint32_t>(RenderFlags::ROUGHNESS_MAPS)) {
         roughnessTex->Bind(5);
         shader->SetUniformTextureSampler2D("u_roughnessMap", 5);
       }
 
-      auto* metalnessTex =
-        model->materials[bm.materialIndex]->m_textures[TEX_TYPE_METALLIC];
+      auto *metalnessTex =
+          model->materials[bm.materialIndex]->m_textures[TEX_TYPE_METALLIC];
       if (metalnessTex &&
-        mr->m_render_flags[bm.materialIndex] &
-        static_cast<uint32_t>(RenderFlags::METALNESS_MAPS)) {
+          mr->m_render_flags[bm.materialIndex] &
+              static_cast<uint32_t>(RenderFlags::METALNESS_MAPS)) {
         metalnessTex->Bind(6);
         shader->SetUniformTextureSampler2D("u_metalnessMap", 6);
       }
 
-      auto* aoTex = model->materials[bm.materialIndex]->m_textures[TEX_TYPE_AO];
+      auto *aoTex = model->materials[bm.materialIndex]->m_textures[TEX_TYPE_AO];
       if (aoTex && mr->m_render_flags[bm.materialIndex] &
-        static_cast<uint32_t>(RenderFlags::AO_MAPS)) {
+                       static_cast<uint32_t>(RenderFlags::AO_MAPS)) {
         aoTex->Bind(7);
         shader->SetUniformTextureSampler2D("u_aoMap", 7);
       }
 
-      auto* emissiveTex =
-        model->materials[bm.materialIndex]->m_textures[TEX_TYPE_EMISSIVE];
+      auto *emissiveTex =
+          model->materials[bm.materialIndex]->m_textures[TEX_TYPE_EMISSIVE];
       if (emissiveTex &&
-        mr->m_render_flags[bm.materialIndex] &
-        static_cast<uint32_t>(RenderFlags::EMISSIVE_MAPS)) {
+          mr->m_render_flags[bm.materialIndex] &
+              static_cast<uint32_t>(RenderFlags::EMISSIVE_MAPS)) {
         emissiveTex->Bind(8);
         shader->SetUniformTextureSampler2D("u_emissiveMap", 8);
       }
@@ -824,28 +826,26 @@ void OpenGLRenderer::RenderMesh(MeshRenderer *mr) {
       // BaseIndex offset.
       if (!model->mesh.m_Indices.empty()) {
         glDrawElementsBaseVertex(
-          GL_TRIANGLES, static_cast<GLsizei>(bm.numIndices), GL_UNSIGNED_INT,
-          (void*)(sizeof(unsigned int) * bm.baseIndex),
-          static_cast<GLint>(bm.baseVertex));
+            GL_TRIANGLES, static_cast<GLsizei>(bm.numIndices), GL_UNSIGNED_INT,
+            (void *)(sizeof(unsigned int) * bm.baseIndex),
+            static_cast<GLint>(bm.baseVertex));
         LogGLErrors("glDrawElementsBaseVertex");
-      }
-      else {
+      } else {
         // No index buffer: draw arrays starting at BaseVertex for NumIndices
         // vertices.
         glDrawArrays(GL_TRIANGLES, static_cast<GLint>(bm.baseVertex),
-          static_cast<GLsizei>(bm.numIndices));
+                     static_cast<GLsizei>(bm.numIndices));
       }
     }
-  }
-  else {
+  } else {
     // Fallback: draw entire mesh if no BasicMesh info available.
     if (!model->mesh.m_Indices.empty())
       glDrawElements(GL_TRIANGLES,
-        static_cast<GLsizei>(model->mesh.m_Indices.size()),
-        GL_UNSIGNED_INT, 0);
+                     static_cast<GLsizei>(model->mesh.m_Indices.size()),
+                     GL_UNSIGNED_INT, 0);
     else
       glDrawArrays(GL_TRIANGLES, 0,
-        static_cast<GLsizei>(model->mesh.m_Positions.size()));
+                   static_cast<GLsizei>(model->mesh.m_Positions.size()));
   }
 
   for (int i = 0; i < 16; ++i) // Assuming 16 texture units (adjust if needed)
